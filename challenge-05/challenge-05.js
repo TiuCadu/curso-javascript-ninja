4aaa6ae898dbd109e2c65637150fbb547ab43a56
Cadu <carlos.olliveira@gmail.com>
2020-06-23 10:46:48 -0400

    challenge-06

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -99,15 +99,11 @@ Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-console.log(
-  "O autor do livro numero 1 é " + book("Livro numero 1").autor + "."
-);
+console.log("O autor do livro numero 1 é " + book("Livro numero 1").autor + ".");
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-console.log(
-  "O livro numero 1 foi publicado pela editora" + book("Livro numero 1").editora + "."
-);
+console.log("O livro numero 1 foi publicado pela editora" + book("Livro numero 1").editora + ".");
--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = "Campeonato Paulista";
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,9 +13,14 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
-
-console.log( 'Times que estão participando do campeonato:', teams );
+var teams = [
+  null,
+  "Palmeiras",
+  "Ponte Preta",
+  "Santos",
+  "Guarani",
+  "São Paulo",
+];
 
 /*
 Crie uma função chamada `showTeamPosition` com as seguintes características:
@@ -32,19 +38,33 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition(number) {
+  if (number <= 5) {
+    return (
+      "O time que está em " + number + "º lugar é o " + teams[number] + "."
+    );
+  } else {
+    return "Não temos a informação do time que está nessa posição.";
+  }
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+console.log(showTeamPosition(1));
+console.log(showTeamPosition(2));
+console.log(showTeamPosition(3));
+console.log(showTeamPosition(6));
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var counter = 20;
+while (counter <= 30) {
+  console.log(counter++);
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +78,35 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
-
+function convertToHex(color) {
+  switch (color.toLowerCase()) {
+    case "vermelho":
+      console.log("O hexadecimal para a cor " + color + " é FF0000.");
+      break;
+    case "preto":
+      console.log(`O hexadecimnal para a cor ${color} é 000000`);
+      break;
+    case "amarelo":
+      console.log(`O hexadecimal para a cor ${color} é FFFF00`);
+      break;
+    case "azul":
+      console.log(`O hexadecimal para a cor ${color} é 0000FF`);
+      break;
+    case "roxo":
+      console.log(`O hexadecimal para a cor ${color} é 993399`);
+      break;
+    default:
+      console.log("Não temos o equivalente hexadecimal para " + color);
+  }
+}
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+console.log(convertToHex('Vermelho'));
+console.log(convertToHex('preto'));
+console.log(convertToHex('amarelo'));
+console.log(convertToHex('azul'));
+console.log(convertToHex('roxo'));
+console.log(convertToHex('Ssdasd'));
+console.log(convertToHex('asdasdasdasd'));
+console.log(convertToHex('asdasdasdasdasdasd'));
